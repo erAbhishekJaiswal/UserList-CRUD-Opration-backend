32edae1b6ee651763b96ef7423b3e71771e16018
Abhishek Jaiswal <er.abhishek72@gmail.com>
2025-03-29 15:04:44 +0530

    cors Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // Replace with your frontend URL
+    origin: 'https://user-list-crud-opration.vercel.app/', // Replace with your frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 };
